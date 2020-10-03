4455c77032b9706c387730576d6d58c811ddce79
97GRC <campos.gab@usp.br>
2020-10-03 03:34:09 +0000

    questao2.2

---
+++ bash_script.sh
@@ -0,0 +1,9 @@
+#Indicando a pasta onde estou:
+pwd
+
+
+#Gerando uma lista da minha pasta HOME, indicando tudo o que há na pasta (a), em forma de lista (l) e organizado (F) e colocando o output no arquivo .txt
+ls -alF /home/gcam > lista_HOME.txt
+
+#Chamando o arquivo para confirmação
+cat lista_HOME.txt
